4c6fdd02ec96350f7176e764624724fcf235cf45
Tavishi <tavishikashyap02@gmail.com>
2026-09-14 00:25:09 +0530

    peak element in array

---
+++ Search/peak.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+
+        if(nums.size() == 1){
+            return 0;
+        }
+
+            
+        if(nums[0] > nums[1]){
+            return 0;
+        }
+
+        for(int i=1; i<nums.size() - 1; i++){
+
+            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+                return i;
+            }
+        }
+
+        if(nums[nums.size()-1] > nums[nums.size()-2]){
+            return nums.size()-1;
+        }
+
+    return -1;
+    }
+};
+
+int main(){
+
+    Solution s;
+    vector<int> nums = {1,2,3,1};
+    cout<<s.findPeakElement(nums)<<endl;
+
+    return 0;
+}
