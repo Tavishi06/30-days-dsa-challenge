6afff443319ffa662097ab256b56feeaf7377b09
Tavishi06 <tavishikashyap02@gmail.com>
2026-09-23 18:29:23 +0530

    Insertion sort

---
+++ Sort/insertion.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    cout << "Enter the number of elements: ";
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+
+    cout << "Enter the elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    // Insertion sort
+
+    for(int i=1; i<n; i++){
+
+        int key = a[i];
+        int j = i-1;
+
+        while(j>=0 && a[j]>key){
+
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = key;
+    }
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
